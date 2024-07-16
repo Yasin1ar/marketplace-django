acf882cc9e19e29b775a102d7e723a1d9d7a60f7
yasin1ar <yasin.amirany@gmail.com>
2024-07-16 21:04:21 +0330

    crucial commit

--- YasMobile/asgi.py
+++ YasMobile/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for YasAccessory project.
+ASGI config for YasMobile project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasAccessory.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMobile.settings')
 
 application = get_asgi_application()
--- YasMobile/wsgi.py
+++ YasMobile/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for YasAccessory project.
+WSGI config for YasMobile project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasAccessory.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMobile.settings')
 
 application = get_wsgi_application()
