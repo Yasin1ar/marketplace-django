d30c4a3b4047a67bb762bdd51044e6774bf0d05c
yasin1ar <yasin.amirany@gmail.com>
2024-07-19 04:46:24 +0330

    initial

--- YasMarket/asgi.py
+++ YasMarket/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for YasMobile project.
+ASGI config for YasMarket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMobile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMarket.settings')
 
 application = get_asgi_application()
--- YasMarket/wsgi.py
+++ YasMarket/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for YasMobile project.
+WSGI config for YasMarket project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMobile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YasMarket.settings')
 
 application = get_wsgi_application()
